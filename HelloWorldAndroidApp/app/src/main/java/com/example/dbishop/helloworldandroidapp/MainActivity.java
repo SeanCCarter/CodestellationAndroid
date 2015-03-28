ab4f2a02a9ed9dbeaf9df6551675cbc2fdffdb5e
Joey L. Maalouf <joeylmaalouf@gmail.com>
2015-03-27 22:57:39 -0400

    breaking stuff

--- HelloWorldAndroidApp/app/src/main/java/com/example/dbishop/helloworldandroidapp/MainActivity.java
+++ HelloWorldAndroidApp/app/src/main/java/com/example/dbishop/helloworldandroidapp/MainActivity.java
@@ -12,48 +12,64 @@ import android.view.MotionEvent;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity
+{
     MyView view;
-
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         view = new MyView(this);
         setContentView(view);
+
+        Runnable updater = new Runnable(){
+            @Override
+            public void run()
+            {
+                view.invalidate();
+                view.postDelayed(updater, 1000/30);
+            }
+        };
+        view.post(updater);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
+        {
             case MotionEvent.ACTION_DOWN:
                 view.touch_x = (int)event.getX();
                 view.touch_y = (int)event.getY();
+                view.radius = 0;
                 break;
 //            case MotionEvent.ACTION_MOVE:
 //            case MotionEvent.ACTION_UP:
         }
-        view.invalidate();
+        view.invalidate();  // Force redrawing.
         return false;
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
@@ -62,22 +78,23 @@ public class MainActivity extends ActionBarActivity {
 
     public class MyView extends View
     {
-
         int touch_x = 200;
         int touch_y = 200;
+        int radius = 0;
+        Paint paint = new Paint();
 
         public MyView(Context context)
         {
             super(context);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.BLACK);
         }
 
         @Override
         protected void onDraw(Canvas canvas)
         {
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLACK);
-            canvas.drawCircle(touch_x, touch_y, 50, paint);
+            canvas.drawCircle(touch_x, touch_y, ++radius, paint);
+            postInvalidateDelayed(1000/30);
         }
     }
 }
